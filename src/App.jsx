7d8f1c290b87804c2ab74ebdcd6bad1ba45b3c6d
r-heart <rachaelkheart@gmail.com>
2023-06-26 17:48:18 -0700

    feat: :sparkles: add useJoke to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import JokeButton from "./components/JokeButton";
 import JokeList from "./components/JokeList";
 import JokeText from "./components/JokeText";
 import JokeVote from "./components/JokeVote";
+import useJoke from "./hooks/useJoke";
 
 function App() {
+  const { joke, setJoke, goodJokes, badJokes, handleVote } = useJoke();
+
   return (
-    <main className="container flex items-center">
-      <section>
+    <main className="container mx-auto flex flex-col items-center gap-y-8">
+      <section className="flex flex-col items-center gap-y-8 border-y py-8">
         <JokeButton setJoke={setJoke} />
-        <JokeText joke={JokeText} />
-        {JokeText && <JokeVote onClick={handleVote} />}
+        <JokeText joke={joke} />
+        {joke && <JokeVote onClick={handleVote} />}
       </section>
-      <section>
+      <section className="grid grid-cols-2 divide-x [&>*]:px-8">
         <JokeList title="👍🏾 Jokes 😆" jokes={goodJokes} />
         <JokeList title="👎🏾 Jokes 😿" jokes={badJokes} />
       </section>

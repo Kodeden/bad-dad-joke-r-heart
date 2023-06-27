02b774757fafbfe0adee5e7db714837ada8c6b48
r-heart <rachaelkheart@gmail.com>
2023-06-26 17:29:21 -0700

    refactor: :recycle: build static version in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import JokeButton from "./components/JokeButton";
+import JokeList from "./components/JokeList";
+import JokeText from "./components/JokeText";
+import JokeVote from "./components/JokeVote";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="container flex items-center">
+      <section>
+        <JokeButton setJoke={setJoke} />
+        <JokeText joke={JokeText} />
+        {JokeText && <JokeVote onClick={handleVote} />}
+      </section>
+      <section>
+        <JokeList title="👍🏾 Jokes 😆" jokes={goodJokes} />
+        <JokeList title="👎🏾 Jokes 😿" jokes={badJokes} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
